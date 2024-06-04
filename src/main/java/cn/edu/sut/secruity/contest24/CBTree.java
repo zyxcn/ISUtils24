017375842916ba30e2a819f7cbb459b92bb001e3
cnzyx <i@webzyx.cn>
2024-06-04 18:52:39 +0800

    Revert "commit local files"
    This reverts commit 9e9a406e4f565af5bb821a64763c930793a541a7.

--- src/main/java/cn/edu/sut/secruity/contest24/CBTree.java
+++
@@ -1,5 +0,0 @@
-package cn.edu.sut.secruity.contest24;
-
-public class CBTree {
-
-}
--- src/main/java/cn/edu/sut/secruity/contest24/TToken.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.sut.secruity.contest24;
-
-import it.unisa.dia.gas.jpbc.Element;
-
-public class TToken {
-    public Element[] tToken;
-    public Element[] f;
-}
